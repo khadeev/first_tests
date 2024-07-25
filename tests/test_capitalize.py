0928f906cb413700955379a69ad5d99ca11b3e9a
yaroslav <yaroslav.khadeev@gmail.com>
2024-07-25 14:55:30 +0300

    Change manual to assertion tests

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,9 +1,5 @@
 from capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-	raise Exception('Функция работает неверно!')
+assert capitalize('hello') == 'Hello'
 
-if capitalize('') != '':
-	raise Exception('Функция работает неверно!')
-
-print('Все тесты пройдены!')
+assert capitalize('') == ''
